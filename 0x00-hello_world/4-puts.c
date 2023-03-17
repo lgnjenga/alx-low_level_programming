b661d68bb5f0de57105dc9c036abf5894877f656
lgnjenga <lgnjenga@gmail.com>
2023-03-17 04:01:22 +0300

    4. Cprogram puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* String declaration: Array of chars  */
+	char string1[] = "Programming is like building a multilingual puzzle";
+
+	/* print to standard output */
+	puts(string1);
+
+	return (0);
+}

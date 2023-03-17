6d49327d4e15a85d889f1a3d8d799f760dd9eabc
lgnjenga <lgnjenga@gmail.com>
2023-03-17 05:12:18 +0300

    7. C assembly code using intel syntax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %li byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %lli byte(s)\n", sizeof(llint1));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(llint1));
 	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 
 	return (0);

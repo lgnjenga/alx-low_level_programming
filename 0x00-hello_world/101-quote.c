6935d60f29e97778abd8918913975c45d0ae5e2e
lgnjenga <lgnjenga@gmail.com>
2023-03-17 05:40:27 +0300

    8. print without using puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,22 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return 1 (Success)
  */
 int main(void)
 {
+	/* print using write */
+	char *string1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t num1 = 0;
+
+	/* while loop */
+	while (string1[num1] != '\0')
+	{
+		num1++;
+	}
+	write(2, string1, num1);
+
+	return (1);
 }

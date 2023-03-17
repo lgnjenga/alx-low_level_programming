1943bd4062534c5aefbd360b18020be0808738e4
lgnjenga <lgnjenga@gmail.com>
2023-03-18 02:05:05 +0300

    2. C Program: for loop:- print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declarations  */
+	char letter;
+
+	/* For loop  */
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	return (0);
+}

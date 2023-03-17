3bc61e768238b3237abbb237767062f98f500d3f
lgnjenga <lgnjenga@gmail.com>
2023-03-17 04:15:40 +0300

    5. printf function for standard output print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/* use printf to directly print to standard output  */
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }

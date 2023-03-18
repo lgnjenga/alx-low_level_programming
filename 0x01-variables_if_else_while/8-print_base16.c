f4595e2e130e2e3ecfbd9e6b0e2f1db3b20f3d9e
lgnjenga <lgnjenga@gmail.com>
2023-03-18 04:07:38 +0300

    8. C Program: for loop, if else statement:- print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declarations  */
+	int hex_num;
+
+	/* For loop  */
+	for (hex_num = 0; hex_num < 16; hex_num++)
+	{
+		/* if statement */
+		if (hex_num < 10)
+		{
+			putchar(hex_num + '0');
+		}
+		else
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

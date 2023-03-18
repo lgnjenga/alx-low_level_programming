33ae5cd20a96360f42d56b0e78b229ba905a5dd4
lgnjenga <lgnjenga@gmail.com>
2023-03-18 03:36:39 +0300

    5. C Program: for loop:- print base 10 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declarations  */
+	int base_num;
+
+	/* For loop  */
+	for (base_num = 0; base_num < 10; base_num++)
+	{
+		printf("%d", base_num);
+	}
+
+	printf("\n");
+
+	return (0);
+}

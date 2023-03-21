6a0956fdd1dc074cefe06b06c47ac20c06beac82
lgnjenga <lgnjenga@gmail.com>
2023-03-22 02:56:48 +0300

    1. C Function code: print alphabet using for loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet a to z using for loop
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}

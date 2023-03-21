4e8bc6cf7d7a6a3bbbb05e0e681113a9446cf4a6
lgnjenga <lgnjenga@gmail.com>
2023-03-21 04:43:36 +0300

    100. C Program: 2 digit combination using nested loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declarations  */
+	int a;
+	int b;
+
+	/* For loop  */
+	for (a = 0; a < 10; a++)
+	{
+		/* Inner For Loop  */
+		for (b == a + 1; b < 10; b++)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+			if (a != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

f2b32738c402363744d926080b99210205b735dd
lgnjenga <lgnjenga@gmail.com>
2023-03-23 20:14:01 +0300

    0x03. C - Debugging

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle using #
+ *
+ * @size: parameter for size of triangle
+ *
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+
+}

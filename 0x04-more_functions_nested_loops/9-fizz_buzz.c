b51eac7f09c35e8804664b18b69c8bc7992eac2c
lgnjenga <lgnjenga@gmail.com>
2023-03-23 16:22:24 +0300

    9. C Program: FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -19,16 +23,11 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
 		else
 		{
 			printf("%d ", i);
 		}
 	}
-	printf("\n");
 
 	return (0);
 }

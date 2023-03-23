fa21b49d75a6fbe047af8cd154110ef32d403bd1
lgnjenga <lgnjenga@gmail.com>
2023-03-23 22:32:02 +0300

    2. C Debugging: check for largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,27 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (a > c && c > b)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else if (b > c && c > a)
+	{
+		largest = b;
+	}
+	else if (c > a && a > b)
+	{
+		largest = c;
+	}
+	else if (c > b && b > a)
 	{
 		largest = c;
 	}

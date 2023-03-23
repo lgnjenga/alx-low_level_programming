0f91324f595de12e0ca29e16ac64e6dd22afd876
lgnjenga <lgnjenga@gmail.com>
2023-03-23 11:45:06 +0300

    4. C Programming Function code: print (0 - 9) using for loop except numbers 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 using for loop except for 2 and 4
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		if (m != 2 && m != 4)
+		{
+			_putchar(m);
+		}
+	}
+	_putchar('\n')
+}

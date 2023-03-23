cb48d8f6c030e5e937f833ae32d5e4c9e9112754
lgnjenga <lgnjenga@gmail.com>
2023-03-23 05:12:59 +0300

    11: C Function code: print all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,6 +19,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf(", ");
 		}
+		printf("\n");
 	}
 	else if (n > 98)
 	{
@@ -28,7 +29,9 @@ void print_to_98(int n)
 			if (i != 98)
 				printf(", ");
 		}
+		printf("\n");
 	}
 	else if (n == 98)
 		printf("%d", n);
+		printf("\n");
 }

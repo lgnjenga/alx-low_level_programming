5a349577d2d7bbec2ddb02234506767efa5633d3
lgnjenga <lgnjenga@gmail.com>
2023-03-23 15:10:34 +0300

    7. C Programming Function code: print diagonal line using backslash n times with for loop

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line using \
+ *
+ * @n: parameter for number of times to print 
+ *
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i < n; i++)
+		{
+			for (j = 0; j < i + 1; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}

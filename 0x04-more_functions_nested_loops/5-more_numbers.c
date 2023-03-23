80e932b9d1aa93875f1cb8443c6297c8248cc01f
lgnjenga <lgnjenga@gmail.com>
2023-03-23 12:58:33 +0300

    5. C Programming Function code: print (0 - 9)10 times using nested for loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers (0 - 14) 10 times using nested for loops
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}

fab419a8615fb73ae0ed1ef778ce57e10ddf7632
lgnjenga <lgnjenga@gmail.com>
2023-03-23 15:47:32 +0300

    8. C Programming Function code: print square using # n times with for loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using #
+ *
+ * @size: parameter for number of times to print
+ *
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

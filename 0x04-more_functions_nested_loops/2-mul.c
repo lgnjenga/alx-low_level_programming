60ad50eb2d275b10988da1f602d717dc6aea100e
lgnjenga <lgnjenga@gmail.com>
2023-03-23 11:18:02 +0300

    2. C Programming Function code: return product of 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ *
+ * @a: function parameter 1
+ * @b: function parameter 2
+ *
+ * Return: a * b which is product of the 2 parameters
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

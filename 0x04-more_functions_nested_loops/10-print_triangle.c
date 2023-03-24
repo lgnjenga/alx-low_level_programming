450740908f36699e8a101a76c731e318133b39ad
lgnjenga <lgnjenga@gmail.com>
2023-03-24 05:26:35 +0300

    10. C Function code: print right sided right hand triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,16 +14,23 @@ void print_triangle(int size)
 	int j;
 	int k;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			_putchar(' ');
-		}
-		for (k = 0; k <= i; k++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }

3659e80b9e0f6fc22f8472d72548e9069c75db5f
lgnjenga <lgnjenga@gmail.com>
2023-03-30 22:36:48 +0300

    4. C Function code - reverse array of integers.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n--; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		j = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = j;
 	}
 }

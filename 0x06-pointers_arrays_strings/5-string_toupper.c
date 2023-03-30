5b23a1cee8e06e8c2359a4eb374dcd82257497cf
lgnjenga <lgnjenga@gmail.com>
2023-03-30 23:20:58 +0300

    5. C Function code - convert string array to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0', i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;

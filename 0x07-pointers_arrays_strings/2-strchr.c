c86c49f4cecc82d553ec9264f461df3b635143f7
lgnjenga <lgnjenga@gmail.com>
2023-04-03 22:53:50 +0000

    2. (strchr) Function Code: find occurrence of character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - returns the 1st occurence of a character
+ *
+ * @s: character array to be searched
+ * @c: character to check
+ *
+ * Return: pointer to occurence or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}

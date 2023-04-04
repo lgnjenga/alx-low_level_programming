00c153932b940bc34b83490d19f2e850b6621006
lgnjenga <lgnjenga@gmail.com>
2023-04-04 22:07:53 +0000

    5. C Function Code: find the 1st occurence of the substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the 1st occurence of the substring
+ *
+ * @haystack: string to be searched
+ * @needle: string to use to search
+ *
+ * Return: pointer to 1st occurence or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n != '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (NULL);
+}

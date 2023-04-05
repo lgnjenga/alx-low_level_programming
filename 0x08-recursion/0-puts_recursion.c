c3e95a806525ae1dffea4af700fc0456da9dca0d
lgnjenga <lgnjenga@gmail.com>
2023-04-05 18:01:45 +0000

    0. C Recursion Function Code: print string like puts function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string followed by a new line
+ * @s: string to print
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+	return;
+}

1cb396d17f8337a84935c9229fd51654137af133
lgnjenga <lgnjenga@gmail.com>
2023-04-05 22:17:27 +0000

    5. C Recursion Function Code: calculate square root of number using recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int sqrt_fn(int b, int e);
+
+/**
+ * _sqrt_recursion - return square root natural number
+ * @n: number to check
+ *
+ * Return: square root natural number
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_fn(n, 1));
+}
+
+/**
+ * sqrt_fn - calculate the natural square root
+ * @b: base number
+ * @e: exponent
+ *
+ * Return: square root natural number
+ */
+int sqrt_fn(int b, int e)
+{
+	int r = e * e;
+
+	if (r > b)
+		return (-1);
+	if (r == n)
+		return (b);
+	return (sqrt_fn(b, e + 1));
+}

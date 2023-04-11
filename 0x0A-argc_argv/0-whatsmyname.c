09b0747ac57d67ba01ae7953d777d2db2fd26f7f
lgnjenga <lgnjenga@gmail.com>
2023-04-11 00:28:24 +0000

    0. print filename frm arguments in main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,15 +10,8 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	char *file_name = strchr(argv[0], '/');
 
-	/* Check if filename has / */
-	if (file_name == NULL)
-		file_name = argv[0];
-	else
-		file_name += 1;
-
-	printf("%s\n", file_name);
+	printf("%s\n", *argv);
 
 	return (0);
 }

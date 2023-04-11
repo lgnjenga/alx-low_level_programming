0a46d3d8bf8b79abf7c18599f871e6c5ca0c937d
lgnjenga <lgnjenga@gmail.com>
2023-04-11 00:22:45 +0000

    0. print filename frm arguments in main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	char *file_name = strchr(argv[0], '/');
 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 

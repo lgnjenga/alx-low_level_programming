0b1a8c32e3697199de4c2d4add1f29f7b39c2b5e
lgnjenga <lgnjenga@gmail.com>
2023-04-12 01:27:24 +0000

    0x0B. C - malloc, free task Assignment

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point, find change in cents
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}

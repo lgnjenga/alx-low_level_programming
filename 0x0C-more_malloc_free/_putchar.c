18b42e53bf98354bd4643d0aace0e73f04060ee2
lgnjenga <lgnjenga@gmail.com>
2023-04-14 05:19:45 +0300

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _putchar - prints characters
+ *
+ * @c: para1
+ *
+ * Return: int 1 if successful, 0 otherwise
+ */
+int _putchar(char c)
+{
+	/* putchar function code */
+	if (c)
+		return (1);
+	else
+		return (0);
+}

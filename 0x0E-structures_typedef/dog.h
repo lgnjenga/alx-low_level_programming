754a2bef2767ccd9e489d92ff83f069c34ebe01c
lgnjenga <lgnjenga@gmail.com>
2023-04-18 02:40:07 +0300

    0. struct for dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner's name
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif

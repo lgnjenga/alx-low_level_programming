29b9eb8a2f9f61bb14f6fd552940d5b96bb4a9f1
lgnjenga <lgnjenga@gmail.com>
2023-04-28 02:49:17 +0300

    4. frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees space allocated to a linked list
+ *
+ * @head: list_t list to be freed
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}

9140a7d3e2cb2aa1757282f83bb3d3a34c5a2754
lgnjenga <lgnjenga@gmail.com>
2023-05-09 03:33:04 +0300

    0. reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,8 +7,6 @@
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-void cleanup(char *buffer, int fd);
-ssize_t read_file(int fd, char *buffer, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
